d5b4519551668f9fa33d6a54a62c7bf0de050573
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2026-01-10 08:47:08 +0000

    .

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,8 +1,16 @@
+import { Header } from '../../../components'
 import React from 'react'
 
 const Dashboard = () => {
+  const user = {name: 'Sadie'}
   return (
-    <div>Dashboard</div>
+    <main className='dashboard wrapper'>
+      <Header 
+        title={`Welcome ${user?.name ?? 'Guest'} 👋🏻`}
+        description="Track activity, trends and popular destinations in real time"   
+      />
+      Dashboard Page Contents
+    </main>
   )
 }
 
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,7 +3,12 @@ import { Link, NavLink } from 'react-router'
 import { sidebarItems } from '~/constants'
 import { cn } from '~/lib/utils'
 
-const NavItems = () => {
+const NavItems = ({handleClick}: { handleClick?: () => void}) => {
+  const user = {
+    name: 'Sadie',
+    email: 'contact@jsmastery.pro',
+    imageUrl: '/assets/images/david.webp',
+  }
   return (
     <section className='nav-items'>
         <Link to='/' className='link-logo'>
@@ -13,18 +18,42 @@ const NavItems = () => {
         <div className='container'>
           <nav>
             {sidebarItems.map(({ id, href, icon, label}) => (
-              <NavLink to={href} key={id}>
-                {
-                  ({ isActive }: { isActive: boolean }) => (
-                    <div className={cn('group nav-item', {'bg-primary-100 !text-white': isActive})}>
-                      <img src={icon} alt={label} className={`group-hover:brightness-0 size-0 group-hover:invert ${isActive ? 'brightness-0 invert' : 'text-dark-200'}`} />
-                      {label}
-                    </div>
-                  )
-                }
+              <NavLink to={href} key={id} >
+                {({ isActive }: { isActive : boolean}) => (
+                  <div className={cn('group nav-item', {
+                    'bg-primary-100 !text-white': isActive
+                  })} onClick={handleClick}>
+                    <img 
+                      src={icon}
+                      alt={label}
+                      className={`group-hover:brightess-0 size-0 group-hover:invert ${isActive ? 'brightness-0 invert': ' text-dark-200'}`}
+                    />
+                    {label}
+                  </div>
+                )}
+               
               </NavLink>
             ))}
           </nav>
+          <footer className='nav-footer'>
+            <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'Sadie'} />
+            <article>
+              <h2>{user?.name}</h2>
+              <p>{user?.email}</p>
+            </article>
+            <button
+              onClick={() => {
+                console.log('logout')
+              }}
+              className='cursor-pointer'
+            >
+              <img
+                src='/assets/icons/logout.svg'
+                alt='logout'
+                className='size-6'
+              />
+            </button>
+          </footer>
         </div>
     </section>
   )
